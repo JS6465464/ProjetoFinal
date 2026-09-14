feffebe270b9877668719c0a5fb1bebce7932c55
Tomas Amorim <8391211@formacao.iefp.pt>
2026-09-14 10:10:21 +0100

    Estrutura inicial

---
+++ jogos/memoria/jogo.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => { 
+
+    const memoryboard = document.getElementById('memory-board');
+    const restartButton = document.getElementById('restartButton');
+    const attemptsDisplay = document.getElementById('attempts');
+    const difficultyButton = document.getElementById('difficultyButton');
+
+    const facilSymbols = ['🌍', '🚀', '👨‍🚀', '☀️', '🌙',];
+    const dificilSymbols = ['earth emoji', 'rocket emoji', 'astronaut emoji', 'sun emoji', 'moon emoji', 'star emoji', 'galaxy emoji', 'spaceship emoji'];
+});
